278dc6712d01d07807ada20463ee0d902fe8f23b
iagolucas2020 <60789632+iagolucas2020@users.noreply.github.com>
2022-07-15 11:07:36 -0300

    Projeto Atualizado

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,19 +20,21 @@ public class SaleController {
 	@Autowired
 	private SaleServices Services;
 	
+	@Autowired
 	private SmsService smsService;
 	
 	@GetMapping
 	public Page<Sale> findSales(
-			@RequestParam(value="minDate", defaultValue = "") String minDate,
+			@RequestParam(value="minDate", defaultValue = "")String minDate,
 			@RequestParam(value="maxDate", defaultValue = "")String maxDate,
 			Pageable pageable){
 		return Services.findSales(minDate, maxDate, pageable);
 	}
 	
-	@GetMapping("/notification")
-	public void notifySms() {
-		smsService.sendSms();
-	}
 
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+		
+	}
 }
--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -33,11 +33,11 @@ public class Sale {
 		this.id = id;
 	}
 
-	public String getSallerName() {
+	public String getSellerName() {
 		return sellerName;
 	}
 
-	public void setSallerName(String sallerName) {
+	public void setSellerName(String sallerName) {
 		this.sellerName = sallerName;
 	}
 
